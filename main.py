1ee24572f8cde76db023191c6ea95d7408f46da0
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-17 09:55:02 +0500

    added the numeric conversion of the data

--- main.py
+++ main.py
@@ -44,11 +44,16 @@ def get_required_info(raw_data , raw_schema):
 
     return survey_df , schema
 
+def convert_numeric(df , coloumn):
+    df[coloumn] = pd.to_numeric(df[coloumn] , errors="coerce") # this coerece will not raise an error it will simply convert the values into the nums and where error occur it will store Nan
 
 def main():
     survery_raw_df = read_csv("Dataset//results.txt")
     raw_schema = pd.read_csv("Dataset//schema.txt" , index_col="Column").QuestionText
     survey_df , schema = get_required_info(survery_raw_df , raw_schema )
+    convert_numeric(survey_df , "Age1stCode")
+    convert_numeric(survey_df , "YearsCode")
+    convert_numeric(survey_df , "YearsCodePro")
 
 
 if __name__ == "__main__":
