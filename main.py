d3d06e2d04542d64a9155cbbbe49572defb93466
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-17 10:01:16 +0500

    dropped the incorrect selections

--- main.py
+++ main.py
@@ -47,6 +47,10 @@ def get_required_info(raw_data , raw_schema):
 def convert_numeric(df , coloumn):
     df[coloumn] = pd.to_numeric(df[coloumn] , errors="coerce") # this coerece will not raise an error it will simply convert the values into the nums and where error occur it will store Nan
 
+def drop_incorrect(df , coloumn , max = 1000000 , min = 0):
+    df.drop(df[df[coloumn] > max].index , inplace = True)
+    df.drop(df[df[coloumn] < min].index , inplace = True)
+
 def main():
     survery_raw_df = read_csv("Dataset//results.txt")
     raw_schema = pd.read_csv("Dataset//schema.txt" , index_col="Column").QuestionText
@@ -54,7 +58,9 @@ def main():
     convert_numeric(survey_df , "Age1stCode")
     convert_numeric(survey_df , "YearsCode")
     convert_numeric(survey_df , "YearsCodePro")
-
+    drop_incorrect(survey_df , "Age" , 100 , 10 )
+    drop_incorrect(survey_df , "WorkWeekHrs" , 140 )
+    print(survey_df.describe())
 
 if __name__ == "__main__":
     main()
